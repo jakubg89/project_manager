2363db94be6da9ee5bc134813607c4d6d41fd0c9
Jakubg89 <jakubgrzywacz89@gmail.com>
2023-04-12 16:45:36 +0200

    Apps modified, routers created, private routers added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,32 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import PrivateRoute from './utils/PrivateRoute'
 
-import Headers from "./components/Headers";
+import Headers from "./components/Headers"
 
+import Login from './pages/Login'
+import Register from './pages/Register'
+// import HomePage from './pages/HomePage'
+import ProjectList from './pages/ProjectList'
+
+
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <div className="container">
-          <Headers />
+        <div className="app-container">
+
           <Routes>
-            <Route path="/login" element={<Login/>}/>
+            <Route path="/" element={
+                        <PrivateRoute>
+                          <ProjectList />
+                        </PrivateRoute>
+            } />
+            <Route path="/login" element={<Login />}/>
+            <Route path='/register' element={<Register />} />
+
           </Routes>
         </div>
       </AuthProvider>
