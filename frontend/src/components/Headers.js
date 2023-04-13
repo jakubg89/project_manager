0aebcf9dcec13f258da8ec8f4a5ad6a5eb111665
Jakubg89 <jakubgrzywacz89@gmail.com>
2023-04-13 10:21:17 +0200

    Headers created

--- frontend/src/components/Headers.js
+++ frontend/src/components/Headers.js
@@ -1,9 +1,39 @@
 import { Link } from 'react-router-dom'
+import AuthContext from '../context/AuthContext'
+import React, {useContext} from 'react'
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const Headers = () => {
+    let {user, logoutUser} = useContext(AuthContext)
     return (
-      <div>
-        <Link to="/login" >Login</Link>
+      <div className='nav-bar-container'>
+        {/* {user ? (
+                <a href="" onClick={logoutUser}>Logout</a>
+            ): (
+              <></>
+            )} */}
+
+          <Navbar collapseOnSelect expand="sm">
+            <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
+            <Navbar.Collapse id="navbarScroll" className="justify-content-between">
+                <Nav>
+                    <Nav.Link to="/">Home</Nav.Link>
+                    <Nav.Link to="/">Create project</Nav.Link>                    
+                </Nav>
+                <Nav>                    
+                    <NavDropdown title="Account " align="end">
+                      <NavDropdown.Item className='nav-bar-item'>Manage account</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      {/* <NavDropdown.Item onClick={logoutUser}>Separated link</NavDropdown.Item> */}
+                      <NavDropdown.Item className='nav-bar-item'>Logout</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>     
+        </Navbar>
       </div>
     )
   }
