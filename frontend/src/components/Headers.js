dd5a18383c31e52e8d8181e6183d539557a331e7
Jakubg89 <jakubgrzywacz89@gmail.com>
2023-04-13 14:46:29 +0200

    Fixed headers

--- frontend/src/components/Headers.js
+++ frontend/src/components/Headers.js
@@ -21,15 +21,15 @@ const Headers = () => {
             <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
             <Navbar.Collapse id="navbarScroll" className="justify-content-between">
                 <Nav>
-                    <Nav.Link to="/">Home</Nav.Link>
-                    <Nav.Link to="/">Create project</Nav.Link>                    
+                    <Nav.Link as={Link} to="/">Project list</Nav.Link>
+                    <Nav.Link as={Link} to="/create-project">Create project</Nav.Link>                    
                 </Nav>
                 <Nav>                    
                     <NavDropdown title="Account " align="end">
-                      <NavDropdown.Item className='nav-bar-item'>Manage account</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/create-project" className='nav-bar-item'>Manage account</NavDropdown.Item>
                       <NavDropdown.Divider />
-                      {/* <NavDropdown.Item onClick={logoutUser}>Separated link</NavDropdown.Item> */}
-                      <NavDropdown.Item className='nav-bar-item'>Logout</NavDropdown.Item>
+                      <NavDropdown.Item onClick={logoutUser}>Logout</NavDropdown.Item>
+                      {/* <NavDropdown.Item className='nav-bar-item'>Logout</NavDropdown.Item> */}
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>     
