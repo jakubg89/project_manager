fd6b0d0ea19a97edc7cc631963fa636aacae9988
Jakubg89 <jakubgrzywacz89@gmail.com>
2023-04-14 17:16:43 +0200

    Endpoints updated, files formatted

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -28,10 +28,10 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'password', 'gender', 'age', 'phone_number']
+        fields = ["id", "email", "password", "gender", "age", "phone_number"]
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
+        password = validated_data.pop("password")
         user = User(**validated_data)
         user.set_password(password)
         user.save()
@@ -45,16 +45,36 @@ class ProjectSerializer(serializers.ModelSerializer):
     start_date = serializers.DateTimeField(format="%Y-%m-%d")
     end_date = serializers.DateTimeField(format="%Y-%m-%d")
     user_id = serializers.IntegerField(required=True)
+    first_name = serializers.SerializerMethodField()
+    last_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
-        fields = ['id', 'name', 'about', 'status', 'start_date', 'end_date', 'user_id']
+        fields = [
+            "id",
+            "name",
+            "about",
+            "status",
+            "start_date",
+            "end_date",
+            "user_id",
+            "first_name",
+            "last_name",
+        ]
+
+    def get_first_name(self, obj):
+        return obj.user.first_name
+
+    def get_last_name(self, obj):
+        return obj.user.last_name
 
 
 class CommentSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(required=True)
     project_id = serializers.IntegerField(required=True)
-    content = serializers.CharField(max_length=500, style={'base_template': 'textarea.html'})
+    content = serializers.CharField(
+        max_length=500, style={"base_template": "textarea.html"}
+    )
     date_added = serializers.DateTimeField(format="%Y-%m-%d %H:%M", required=False)
 
     class Meta:
