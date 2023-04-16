7cf1aa75fa21fa5f33095558c016abfaecf6c9cc
Jakubg89 <jakubgrzywacz89@gmail.com>
2023-04-16 13:27:50 +0200

    Deleting unneeded code

--- backend/api/views.py
+++ backend/api/views.py
@@ -28,17 +28,13 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 class GenderChoices(APIView):
     def get(self, request):
-        gender = {}
-        for i in User.GENDER_CHOICES:
-            gender.update({i[0]: i[1]})
+        gender = {i[0]: i[1] for i in User.GENDER_CHOICES}
         return Response({"gender_choices": gender})
 
 
 class ProjectChoices(APIView):
     def get(self, request):
-        project = {}
-        for i in Project.STATUS_CHOICES:
-            project.update({i[0]: i[1]})
+        project = {i[0]: i[1] for i in Project.STATUS_CHOICES}
         return Response({"status": project})
 
 
@@ -51,7 +47,7 @@ class UserView(ModelViewSet):
             self.permission_classes = [
                 IsAuthenticated,
             ]
-        elif self.action in ["create"]:
+        elif self.action == "create":
             self.permission_classes = [
                 AllowAny,
             ]
@@ -59,7 +55,7 @@ class UserView(ModelViewSet):
         return super().get_permissions()
 
     def get_queryset(self):
-        if self.action in ["list"]:
+        if self.action == "list":
             self.queryset = User.objects.filter(is_superuser=0)
         elif self.action in ["destroy", "retrieve", "update", "partial_update"]:
             self.queryset = User.objects.filter(id=self.request.user.pk)
@@ -76,14 +72,14 @@ class ProjectView(ModelViewSet):
     ]
 
     def get_queryset(self):
-        if self.action in ["list"]:
-            self.queryset = Project.objects.filter(user=self.request.user)
+        if self.action == "list":
+            queryset = Project.objects.filter(user=self.request.user)
         elif self.action in ["destroy", "retrieve", "update", "partial_update"]:
             project_pk = self.kwargs.get("pk")
-            self.queryset = Project.objects.filter(id=project_pk)
+            queryset = Project.objects.filter(id=project_pk)
         else:
-            self.queryset = None
-        return super().get_queryset()
+            queryset = None
+        return queryset
 
     def create(self, request, *args, **kwargs):
         users_list = request.data["users"]
@@ -106,6 +102,6 @@ class CommentView(GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin
 
     @action(detail=True, methods=["get"], url_path="details", url_name="details")
     def project_comments(self, request, pk=None):
-        self.queryset = Comment.objects.filter(project_id=pk).order_by("-date_added")
-        serializer = self.get_serializer(self.queryset, many=True)
+        queryset = Comment.objects.filter(project_id=pk).order_by("-date_added")
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
